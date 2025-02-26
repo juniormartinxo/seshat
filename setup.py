290a0c712bc762288f6fdc93f38b968d9c3bddf7
Junior Martins <amjr.box@gmail.com>
2025-02-26 13:34:57 -0300

    feat(setup.py): Incrementou a versão para 0.1.1 por causa da nova funcionalidade

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="seshat",
-    version="0.1.0",
+    version="0.1.1",  # Versão incrementada por causa da nova funcionalidade
     packages=find_packages(),
     install_requires=[
         "click>=8.0",
