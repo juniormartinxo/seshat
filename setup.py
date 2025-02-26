4895f6e6e0f59c13e9a8c3850f0d2bd2a279bb08
Junior Martins <amjr.box@gmail.com>
2025-02-26 16:12:23 -0300

    fix: incrementar versão para 0.1.4 devido a correção

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="seshat",
-    version="0.1.3",  # Versão incrementada por causa da nova funcionalidade
+    version="0.1.4",  # Versão incrementada por causa da correção
     packages=find_packages(),
     install_requires=[
         "click>=8.0",
