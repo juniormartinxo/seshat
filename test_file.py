3111d0ef9d5468f2951b9497203728682680e061
Junior Martins <amjr.box@gmail.com>
2025-07-18 19:03:29 -0300

    chore: remover arquivo de teste obsoleto

--- test_file.py
+++
@@ -1,9 +0,0 @@
-# Arquivo de teste para verificar o novo prompt
-def add_new_feature():
-    """Nova funcionalidade para testar o prompt"""
-    return "feature added"
-
-
-def fix_bug():
-    """Correção de bug"""
-    return "bug fixed"
