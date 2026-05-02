a0996a5a75ac8577588ae3ba2b500772c809879c
Junior Martins <amjr.box@gmail.com>
2026-05-01 23:12:49 -0300

    style(site): corrigir formatação de componentes MDX

--- site/mdx-components.tsx
+++ site/mdx-components.tsx
@@ -3,25 +3,25 @@ import type { AnchorHTMLAttributes, ReactNode } from "react";
 import type { MDXComponents } from "mdx/types";
 
 type DocsLinkProps = AnchorHTMLAttributes<HTMLAnchorElement> & {
-  children?: ReactNode;
-  href?: string;
+	children?: ReactNode;
+	href?: string;
 };
 
 function DocsLink({ children, href = "", ...props }: DocsLinkProps) {
-  if (href.startsWith("/")) {
-    return <Link href={href}>{children}</Link>;
-  }
+	if (href.startsWith("/")) {
+		return <Link href={href}>{children}</Link>;
+	}
 
-  return (
-    <a href={href} {...props}>
-      {children}
-    </a>
-  );
+	return (
+		<a href={href} {...props}>
+			{children}
+		</a>
+	);
 }
 
 export function useMDXComponents(components: MDXComponents = {}): MDXComponents {
-  return {
-    a: DocsLink,
-    ...components
-  };
+	return {
+		a: DocsLink,
+		...components
+	};
 }
