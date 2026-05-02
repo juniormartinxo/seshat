4d975d4b09ac668502e3ad022eff0a472584420a
Junior Martins <amjr.box@gmail.com>
2026-05-01 23:12:38 -0300

    refactor(docs): simplificar tratamento de erros e estrutura do componente DocsPage

--- site/app/docs/[[...mdxPath]]/page.tsx
+++ site/app/docs/[[...mdxPath]]/page.tsx
@@ -2,40 +2,40 @@ import { importPage } from "nextra/pages";
 import { notFound } from "next/navigation";
 
 type DocsPageProps = {
-  params: Promise<{
-    mdxPath?: string[];
-  }>;
+	params: Promise<{
+		mdxPath?: string[];
+	}>;
 };
 
 export const dynamic = "force-dynamic";
 
 export async function generateMetadata(props: DocsPageProps) {
-  const params = await props.params;
-  try {
-    const { metadata } = await importPage(params.mdxPath);
-    return metadata;
-  } catch {
-    return {
-      title: "Documentacao | Seshat"
-    };
-  }
+	const params = await props.params;
+	try {
+		const { metadata } = await importPage(params.mdxPath);
+		return metadata;
+	} catch {
+		return {
+			title: "Documentacao | Seshat"
+		};
+	}
 }
 
 export default async function DocsPage(props: DocsPageProps) {
-  const params = await props.params;
-  let page;
+	const params = await props.params;
+	let page;
 
-  try {
-    page = await importPage(params.mdxPath);
-  } catch {
-    notFound();
-  }
+	try {
+		page = await importPage(params.mdxPath);
+	} catch {
+		notFound();
+	}
 
-  const { default: MDXContent } = page;
+	const { default: MDXContent } = page;
 
-  return (
-    <article className="docsArticle">
-      <MDXContent {...props} params={params} />
-    </article>
-  );
+	return (
+		<article className="docsArticle">
+			<MDXContent {...props} params={params} />
+		</article>
+	);
 }
